c9afb8f339c4f04f060590fb26e67fc1ad4efb40
RichStone <richard.had@hotmail.de>
2019-06-01 18:17:55 +0200

    finish getAuthors & start get last 24 hours links

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const fetch = require("node-fetch");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.addAuthor = functions.firestore
+    .document('users/{userId}/trackedAuthors/{newAuthor}')
+    .onCreate((snapshot, context) => {
+        fetch('https://www.thefastlaneforum.com/community/threads/c-programming-books.88834/')
+            .then((response) => {
+                return response.text();
+            })
+            .catch((error) => {
+                console.error("something went wrong: " + error);
+            })
+            .then((body) => {
+                console.log(body);
+                return body
+            })
+            .catch((error) => {
+                console.error("something went wrong: " + error)
+            });
+        console.log(context.params.newAuthor);
+        console.log(snapshot.data().dateCreated);
+        return true
+    });
+
+exports.testo = functions.https.onRequest(async (req, res) => {
+    const original = req.query.text;
+    console.log('goof');
+    res.redirect('https://www.google.com/')
+});
